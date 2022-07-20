e619d9fc00dc99d2d45101d94f3dd5294b5268fc
JulianoManzano <102270138+JulianoManzano@users.noreply.github.com>
2022-07-20 19:37:39 -0400

    Body pronto, fazer filtros e carrinho
    Segue projeto com o Body pronto, focar em filtros e carrinho depois estilização.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React,{useState} from "react";
+import data from './data';
+import Carrinho from "./components/Carrinho";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Filtros from "./components/Filtros";
 
 function App() {
+  const { produtos } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
   return (
     <div className="App">
-     <>
-     
-     </>
-
+      <Header countCartItems={cartItems.length}></Header>
+      <div className="row">
+        <Filtros/>
+        <Home products={produtos} onAdd={onAdd}></Home>
+        <Carrinho
+         
+        ></Carrinho>
+      </div>
     </div>
   );
 }
---
+++ src/components/Carrinho.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Carrinho(props) {
+  const { cartItems, AddCar, onRemove } = props;
+  
+  return (
+    <aside className="block col-1">
+      <h2>Itens do Carrinho</h2>
+      <div>
+       
+      </div>
+    </aside>
+  );
+}
