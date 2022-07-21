76880f9ef913777672e51a5ad799dc3390bb5831
Jessica Oliveira Braga <jessi-oliveira@hotmail.com>
2022-07-21 17:09:20 -0300

    parte2-filtros

--- src/components/Filtros.js
+++ src/components/Filtros.js
@@ -1,11 +1,43 @@
      import React from 'react'
      
-     export function Filtros() {
+     export function Filtros(props) {
 
         
        return(
-        <div className="row">
+        <div className="">
+
+      <input
+         placeholder="Nome do Produto"
+         value={props.query}
+         onChange={(ev) => props.setQuery(ev.target.value)}
+      />
+
+
+         <input
+         type="number"
+         placeholder="Preço mínimo"
+         value={props.minPrice}
+         onChange={(ev) => props.setMinPrice(ev.target.value)}
+        />
+
+
+        <input
+         type="number"
+         placeholder="Preço máximo"
+         value={props.maxPrice}
+         onChange={(ev) => props.setMaxPrice(ev.target.value)}
+        />
         
+      
+        <select
+         value={props.order}
+         onChange={(ev) => props.setOrder(ev.target.value)}
+      >
+         <option value={"asc"}>Crescente</option>
+         <option value={"desc"}>Decrescente</option>
+
+      </select>
+
         </div>
        )    
     };
